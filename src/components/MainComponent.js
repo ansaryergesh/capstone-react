629603836151bbf967903e1ff60832bafc29c0c2
ansaryergesh <yergeshovansar@gmail.com>
2020-05-08 03:17:24 +0600

    meal detail

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Header from './Header';
 import Menu from './Menu';
 import Footer from './Footer';
+import MealDetail from './DetailMenu';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchMeals,} from '../redux/ActionCreators';
@@ -17,6 +18,16 @@ const mapDispathToProps = dispatch => ({
   fetchMeals: () => {dispatch(fetchMeals())},
 });
 
+const MealWithId = ({match}) => {
+    return (
+        <MealDetail 
+        meal={this.props.meals.meals.filter(meal => meal.id === parseInt(match.params.mealId, 10))[0]} 
+        isLoading = {this.props.meals.isLoading}
+        errMess = {this.props.meals.errMess}
+            />
+    );
+}
+
 
 class MainComponent extends Component {
     componentDidMount() {
@@ -30,7 +41,8 @@ class MainComponent extends Component {
             <Header />
             <Switch>
                 <Route path = '/home' component={() => <Home/>}/>
-                <Route path='/menu' component={() => <Menu meals={this.props.meals} />} />
+                <Route exact path='/menu' component={() => <Menu meals={this.props.meals} />} />
+                <Route path="/menu/:mealId" component={MealWithId} />
                 <Route path='/categories' component={() => <Category />} />
                 <Redirect to='/home' />
             </Switch>
