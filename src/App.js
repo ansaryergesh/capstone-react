08c5aebf22605d474159d624420a5502eb31f8f3
ansaryergesh <yergeshovansar@gmail.com>
2020-05-07 16:57:33 +0600

    about component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
-import Main from './Components/Main';
+import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'; 
-import { ConfigureStore } from './redux/configureStore';
-const store = ConfigureStore();
-
 
 class App extends Component {
-
   render() {
     return (
-      <Provider store={store}>
-          <BrowserRouter>
+      <BrowserRouter>
            <div className="App">
             <Main />
           </div>
-        </BrowserRouter>
-      </Provider>
- 
+      </BrowserRouter>
     );
   }
 }
