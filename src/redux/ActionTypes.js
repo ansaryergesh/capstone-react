62cfde907e29146e77df3b349bb37d736bc9d840
ansaryergesh <yergeshovansar@gmail.com>
2020-05-07 17:39:18 +0600

    action types for meals

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,3 +1,3 @@
-export const ADD_TEST = 'ADD_TEST';
-export const TEST_LOADING = 'TEST_LOADING';
-export const TEST_FAILED = 'TEST_FAILED';
+export const ADD_MEALS = 'ADD_MEALS';
+export const MEALS_LOADING = 'MEALS_LOADING';
+export const MEALS_FAILED = 'MEALS_FAILED';
--- src/shared/url.js
+++ src/shared/url.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'https://www.themealdb.com/api/json/v1/1/';
+export const baseUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
