e9a8618a9d97e35bea74dbd5a7b15531295f08e8
ansaryergesh <yergeshovansar@gmail.com>
2020-05-07 17:25:10 +0600

    font awesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
