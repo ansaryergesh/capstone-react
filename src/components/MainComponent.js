dd82472ebcf78690a888705adad2289aa845cd10
ansaryergesh <yergeshovansar@gmail.com>
2020-05-07 17:28:59 +0600

    new components added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import Home from './Home';
 import Header from './Header';
+import Menu from './Menu';
 import Footer from './Footer';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import Category from './Categories';
 
 class MainComponent extends Component {
     render() {
@@ -11,6 +13,8 @@ class MainComponent extends Component {
             <Header />
             <Switch>
                 <Route path = '/home' component={() => <Home/>}/>
+                <Route path='/menu' component={() => <Menu />} />
+                <Route path='/categories' component={() => <Category />} />
                 <Redirect to='/home' />
             </Switch>
             <Footer/>
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Menu extends Component {
+    render() {
+        return(
+            <div>
+                Menu Page
+            </div>
+        )
+    }
+}
+
+export default Menu;
